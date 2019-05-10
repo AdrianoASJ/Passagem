2c5854d5fcb6ed77567bb12dd3c3152855194b38
Adriano Junior <Sousa.junior@deway.com.br>
2019-05-10 15:00:00 -0300

    adicionado label flutuante de nao visto

--- components/Home.js
+++ components/Home.js
@@ -26,16 +26,18 @@ export default class Home extends Component {
       const response = await EstadosApi.json();
       this.setState({ data: response });
       console.log(this.state.data);
-    } catch (err) {
+       } catch (err) {
       console.log("Erro ao pegar os Estados", err);
     }
   }
 
   render() {
     return (
+      <View style={{flex: 1, paddingTop: 20}}>
       <ScrollView style={styles.container}>
         {this.state.data.map((rowdata, i) => (
           <TouchableOpacity key={i} style={styles.caixa}>
+
             <View style={{ flex: 1 }}>
               <Text style={styles.nome}>{rowdata.nome}</Text>
               <Text style={styles.uf}>{rowdata.uf}</Text>
@@ -43,33 +45,55 @@ export default class Home extends Component {
             <View style={{ flex: 1, justifyContent: "center" }}>
               <Text style={styles.nomefilho}>{rowdata.nome}</Text>
             </View>
+            <View style={styles.caixaSuperior}>
+              <Text style={styles.textCaixaSuperior}>Não Visto</Text>
+            </View> 
+
           </TouchableOpacity>
         ))}
       </ScrollView>
+      </View>
     );
   }
 }
-
+//posicao relativa no pai e posição absoluta no filho para criar a caixinha
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    padding: 10,
-    backgroundColor: "#F1F1F1",
+    position: 'relative',
     marginTop: Constants.statusBarHeight
   },
 
   caixa: {
+    margin: 10,
+    backgroundColor: "#117D00",
     flexDirection: "row",
-    marginBottom: 25,
-    backgroundColor: "#2CB8AD",
     borderColor: "gray",
+    marginBottom: 25,
     borderRadius: 10,
     borderWidth: 2
   },
 
+  caixaSuperior:{
+    backgroundColor: "#005CE6",
+    borderColor: '#0E0F1A',
+    position: 'absolute',
+    justifyContent: 'center',
+    borderWidth: 1,
+    borderRadius: 5,
+    top: -10,
+    right: 0,
+  },
+
+  textCaixaSuperior: {
+    color: "#E6EDEF",
+    paddingLeft: 10,
+    paddingRight: 10
+  },
+
   nome: {
-    fontSize: 18,
+    color: "#E6EDEF",
     fontWeight: "bold",
+    fontSize: 18,
     paddingTop: 10,
     paddingLeft: 10,
     paddingBottom: 5
@@ -82,8 +106,8 @@ const styles = StyleSheet.create({
   },
 
   uf: {
+    color: "#9FC6FF",
     paddingLeft: 10,
     paddingBottom: 10,
-    color: "#E6EDEF"
   }
 });
